01211cf911b926508468ba66e0bb036377b68d05
r.aramide <r.aramide@lagetronix.com>
2023-06-07 17:01:56 +0100

    working

---
+++ react-quizzicalTS/src/components/EachQuestion.tsx
@@ -0,0 +1,13 @@
+export function EachQuestion() {
+  return (
+    <div>
+      <h4>Question comes here</h4>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+      </ul>
+    </div>
+  );
+}
