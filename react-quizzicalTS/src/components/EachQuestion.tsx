a0e9402f643d71897e7fe7d485a066cdf0b6226c
r.aramide <r.aramide@lagetronix.com>
2023-06-09 17:03:41 +0100

    m

--- react-quizzicalTS/src/components/EachQuestion.tsx
+++ react-quizzicalTS/src/components/EachQuestion.tsx
@@ -1,12 +1,33 @@
-export function EachQuestion() {
+import { useState } from "react";
+
+export type EachQuestionProps = {
+  key: string;
+  question: string;
+  options: string[];
+  answer: string | boolean;
+};
+
+export function EachQuestion({ question, options, answer }: EachQuestionProps) {
+  const [selectedAns, setSelectedAns] = useState([]);
   return (
-    <div>
-      <h4>Question comes here</h4>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
+    <div className="each-question">
+      <h3>{question}</h3>
+      <ul className="options">
+        {options.map((opt, i) => (
+          // <li key={i}>{opt}</li>
+          <>
+            <input
+              id={opt}
+              className="option-input"
+              type="radio"
+              name={question}
+              value={opt}
+            />
+            <label htmlFor={opt} className="option-input-label">
+              {opt}
+            </label>
+          </>
+        ))}
       </ul>
     </div>
   );
--- react-quizzicalTS/src/components/QuizzPage.tsx
+++ react-quizzicalTS/src/components/QuizzPage.tsx
@@ -1,7 +1,9 @@
 // https://opentdb.com/api.php?amount=5&category=9
 
 import { ReactNode, useEffect, useState } from "react";
-import { EachQuestion } from "./EachQuestion";
+import { EachQuestion, EachQuestionProps } from "./EachQuestion";
+import { nanoid } from "nanoid";
+import { decode } from "html-entities";
 
 export function QuizzPage() {
   const [quizz, setQuizz] = useState([]);
@@ -12,22 +14,48 @@ export function QuizzPage() {
         "https://opentdb.com/api.php?amount=5&category=9"
       );
       const dataJSON = await data.json();
-      setQuizz(dataJSON);
+      setQuizz(() => {
+        return dataJSON.results.map((data: any) => {
+          return {
+            question: decode(data.question),
+            options: shuffleArray([
+              ...data.incorrect_answers,
+              data.correct_answer,
+            ]),
+            answer: decode(data.correct_answer),
+          };
+        });
+      });
     }
     fetchQuizz();
   }, []);
 
-  console.log(quizz);
-  type eachQuizz = string;
+  function shuffleArray(array: string[] | boolean[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    array.forEach((el: string | boolean) => {
+      if (typeof el == "string") return decode(el);
+    });
+    return array;
+  }
+
   return (
     <div className="quizz-page">
       <div className="top-blob"></div>
       <div className="bottom-blob"></div>
       {quizz.map(
-        (eachQuizz): ReactNode => (
-          <EachQuestion eachQuizz={eachQuizz.question} />
+        (eachQuizz: EachQuestionProps): ReactNode => (
+          <EachQuestion
+            key={nanoid()}
+            question={eachQuizz.question}
+            options={eachQuizz.options}
+            answer={eachQuizz.answer}
+          />
         )
       )}
+
       <button className="submit-btn">Check Answers</button>
     </div>
   );
