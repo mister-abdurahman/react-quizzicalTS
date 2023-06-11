507e36913f50a1911c5235a755d43337435a5654
abdurahman <aramramadan7@gmail.com>
2023-06-11 21:32:13 +0100

    working with old style

--- react-quizzicalTS/src/App.tsx
+++ react-quizzicalTS/src/App.tsx
@@ -1,40 +1,116 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+
+import "./App.css";
 import { HomePage } from "./components/HomePage";
-import { QuizzPage } from "./components/QuizzPage";
-import { NoPage } from "./components/NoPage";
-import { ReactNode, useEffect, useState } from "react";
-// import { decode } from "html-entities";
-// import { EachQuestion, EachQuestionProps } from "./components/EachQuestion";
-// import { nanoid } from "nanoid";
+import { nanoid } from "nanoid";
+import { decode } from "html-entities";
+import { EachQuestion } from "./components/EachQuestion";
 
-function App() {
-
-  // const [start, setStart] = useState(false)
-  // const [quizz, setQuizz] = useState([]);
-  // const [restart, setRestart] = useState(false)
-  // const [score, setScore] = useState(0)
-  // const [submitted, setSubmitted] = useState(false)
- 
-  return (
-    <>
-    {/* {!start && <HomePage setStart={setStart} />} */}
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<HomePage />} />
-          <Route path="/start-quizz" element={<QuizzPage />} />
-          <Route path="*" element={<NoPage />} />
-          <Route />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+type quizzProps = {
+  question: string
+  incorrect_answers: string[]
+  correct_answer: string
 }
+type eachQuizzProps = {
+  eachQuestionId: string
+  question: string
+  options: string[]
+}
+interface selectedOptionProps extends eachQuizzProps
+ {
+  selectedAns: string
+  answer: string
+} 
+export default function App() {
+  const [start, setStart] = useState(false);
+  const [quizz, setQuizz] = useState([]);
 
-export default App;
+  const [playAgain, setPlayAgain] = useState(false);
+  const [numOfCorrect, setNumOfCorrect] = useState(0);
+
+  const markSelected = function(event: React.ChangeEvent<HTMLInputElement>) {
+    const theSelectedOption:selectedOptionProps = quizz.find(
+      (el:eachQuizzProps) => el.eachQuestionId === event.target.name
+      )!;
+      theSelectedOption.selectedAns = event.target.value;
+      console.log(quizz)
+    }
+
+    function checkAnswers(){
+      quizz.forEach((el: selectedOptionProps)=>{
+        if(el.selectedAns === el.answer) {
+          setNumOfCorrect((prev)=>{
+            return prev + 1
+          })
+        }
+        
+        if(el.selectedAns !== el.answer) {
+          return document.getElementsByName(el.eachQuestionId).forEach((eachInput:any)=>{
+            if(eachInput.value === el.answer){
+              eachInput.classList.add('correct-answer')
+            }
+          })
+        }
+      })
+      setPlayAgain(true)
+    }
+    function playAgainFn(){
+      setStart(false)
+      setPlayAgain(false)
+    }
+
+  useEffect(() => {
+    async function getQuizz() {
+      const res = await fetch('https://opentdb.com/api.php?amount=5')
+      const data = await res.json()
+
+      setQuizz(data.results.map((data: quizzProps) => {
+        return {
+          eachQuestionId: nanoid(),
+          question: decode(data.question),
+          options: shuffleArray([
+            ...data.incorrect_answers,
+            data.correct_answer,
+          ]),
+          answer: data.correct_answer
+        };
+      }))
+      console.log(quizz)
+    }
+    getQuizz()
+  }, [start])
+
+  const quizzArray = quizz.map((eachquizz: eachQuizzProps) => {
+      return (
+        <EachQuestion 
+        id={eachquizz.eachQuestionId}
+        question={eachquizz.question}
+        options={eachquizz.options}
+        markSelected={markSelected}
+        />
+      )
+  })
+
+  function shuffleArray(array: string[] | boolean[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    array.forEach((el: any) => decode(el))
+    return array;
+  }
+
+  return (
+    <main>
+      {!start && <HomePage setStart={setStart} id={nanoid()} />}
+      {start && <div className="quizz-wrapper"> {quizzArray} </div>}
+      {!playAgain && <button className="submit-btn" onClick={checkAnswers}>Check Answers</button>}
+      {playAgain && 
+      <div className="btn-flex">
+        <button className="play-again-btn" onClick={playAgainFn}>Play Again</button>
+        <h4 className="text">You scored {numOfCorrect} of {quizz.length}</h4>
+      </div>
+        }
+    </main>
+  )
+}
--- react-quizzicalTS/src/components/HomePage.tsx
+++ react-quizzicalTS/src/components/HomePage.tsx
@@ -1,17 +1,16 @@
 import { Link } from "react-router-dom";
-// type startQuizProps = {setStart: React.Dispatch<React.SetStateAction<boolean>>}
-// export function HomePage({setStart}: startQuizProps) {
-export function HomePage() {
+type startQuizProps = {setStart: React.Dispatch<React.SetStateAction<boolean>>, id: string}
+export function HomePage({setStart}: startQuizProps) {
   return (
     <div className="home">
       <div className="top-blob"></div>
       <div className="bottom-blob"></div>
       <h3 className="head-text">Quizzical</h3>
       <p className="description">Play this game to test your IQ power 🔥</p>
-      <Link to={"./start-quizz"}>
-        {/* <button className="start-btn" onClick={() => setStart(true)}>Start Quiz</button> */}
-        <button className="start-btn">Start Quiz</button>
-      </Link> 
+      {/* <Link to={"./start-quizz"}> */}
+        <button className="start-btn" onClick={() => setStart(true)}>Start Quiz</button>
+        {/* <button className="start-btn">Start Quiz</button> */}
+      {/* </Link>  */}
     </div>
   );
 }
