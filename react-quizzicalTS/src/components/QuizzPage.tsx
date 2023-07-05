cdf587edf38c034decfa922815a9f8250cbee9c9
r.aramide <r.aramide@lagetronix.com>
2023-07-05 12:37:32 +0100

    build

--- react-quizzicalTS/src/components/QuizzPage.tsx
+++ react-quizzicalTS/src/components/QuizzPage.tsx
@@ -4,7 +4,7 @@ import { ReactNode, useEffect, useRef, useState } from "react";
 import { EachQuestion, EachQuestionProps } from "./EachQuestion";
 import { nanoid } from "nanoid";
 import { decode } from "html-entities";
-import '../index.css'
+import "../index.css";
 
 // type quizzPageProps = {
 //   score: number
@@ -14,46 +14,47 @@ import '../index.css'
 // }
 export function QuizzPage() {
   const [quizz, setQuizz] = useState([]);
-  const [restart, setRestart] = useState(false)
-  const [score, setScore] = useState(0)
-  const [submitted, setSubmitted] = useState(false)
+  const [restart, setRestart] = useState(false);
+  const [score, setScore] = useState(0);
+  const [submitted, setSubmitted] = useState(false);
 
-  const markSelected = function (e: React.ChangeEvent<HTMLInputElement>){
-    quizz.forEach((el: EachQuestionProps)=>{
-      if(e.target.name === el.question) {
-        el.selectedAns = e.target.value
+  const markSelected = function (e: React.ChangeEvent<HTMLInputElement>) {
+    quizz.forEach((el: EachQuestionProps) => {
+      if (e.target.name === el.question) {
+        el.selectedAns = e.target.value;
       }
-    })
-  }
+    });
+  };
 
-  const submitQuizz = function(){
-    quizz.forEach((eachQuizz: EachQuestionProps)=>{
-   
-      if(eachQuizz.answer === eachQuizz.selectedAns) {
-        setScore((prev)=> {
-        return prev + 1
-      }) 
-    }
-    
-      if(eachQuizz.selectedAns !== eachQuizz.answer) {
-        return document.getElementsByName(eachQuizz.question).forEach((eachInput:any)=>{
-          if(eachInput.value === eachQuizz.answer){
-            eachInput.classList.add('correct-answer')
-          }
-        })
+  const submitQuizz = function () {
+    quizz.forEach((eachQuizz: EachQuestionProps) => {
+      if (eachQuizz.answer === eachQuizz.selectedAns) {
+        setScore((prev) => {
+          return prev + 1;
+        });
+      }
+
+      if (eachQuizz.selectedAns !== eachQuizz.answer) {
+        return document
+          .getElementsByName(eachQuizz.question)
+          .forEach((eachInput: any) => {
+            if (eachInput.value === eachQuizz.answer) {
+              eachInput.classList.add("correct-answer");
+            }
+          });
       }
       // return document.getElementsByName(`${eachQuizz.question}`).forEach((el: any)=>{
       //     if(eachQuizz.answer === el.value) el.classList.add('correct-answer')
       //   })
-    })
-    setSubmitted(true)
-  }
- 
-  const playAgain = function(){
-    setSubmitted(false)
-    setScore(0) 
-    setRestart(true)
-   }
+    });
+    setSubmitted(true);
+  };
+
+  const playAgain = function () {
+    setSubmitted(false);
+    setScore(0);
+    setRestart((v) => !v);
+  };
 
   useEffect(() => {
     async function fetchQuizz() {
@@ -66,30 +67,31 @@ export function QuizzPage() {
           return {
             question: decode(data.question),
             options: shuffleArray([
-             ...data.incorrect_answers,
+              ...data.incorrect_answers,
               data.correct_answer,
             ]),
             answer: decode(data.correct_answer),
           };
         })
-      )
+      );
     }
     fetchQuizz();
-  }, []);
+  }, [restart]);
 
-  const quizzArray = quizz.map(
-    (eachQuizz: EachQuestionProps): ReactNode => {
-     return (<EachQuestion
-         key={nanoid()}
-         question={eachQuizz.question}
-         options={eachQuizz.options}
-         answer={eachQuizz.answer}
-         markSelected={markSelected}
-         selectedAns={eachQuizz.selectedAns}
-       />)}
-   )
+  const quizzArray = quizz.map((eachQuizz: EachQuestionProps): ReactNode => {
+    return (
+      <EachQuestion
+        key={nanoid()}
+        question={eachQuizz.question}
+        options={eachQuizz.options}
+        answer={eachQuizz.answer}
+        markSelected={markSelected}
+        selectedAns={eachQuizz.selectedAns}
+      />
+    );
+  });
 
- const shuffleArray = function(array: string[] | boolean[]) {
+  const shuffleArray = function (array: string[] | boolean[]) {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
@@ -98,7 +100,7 @@ export function QuizzPage() {
       if (typeof el == "string") return decode(el);
     });
     return array;
-  }
+  };
 
   // const markSelected = function (event: React.ChangeEvent<HTMLInputElement>) {
   //   const theSelectedOption: {selectedAns: string} = quizz.find(
@@ -112,14 +114,24 @@ export function QuizzPage() {
   return (
     <div className="quizz-page">
       <div className="top-blob"></div>
-       <div className="bottom-blob"></div>
+      <div className="bottom-blob"></div>
       {quizzArray}
-      {!submitted && <button className="submit-btn" onClick={submitQuizz}>Check Answers</button>}
+      {!submitted && (
+        <button className="submit-btn" onClick={submitQuizz}>
+          Check Answers
+        </button>
+      )}
 
-      {submitted && <div className="submitted">
-       <h4>You scored {score} of {quizz.length} questions</h4>
-      <button className="submit-btn play-again" onClick={playAgain}>Play Again</button> 
-      </div>}
-     </div>
+      {submitted && (
+        <div className="submitted">
+          <h4>
+            You scored {score} of {quizz.length} questions
+          </h4>
+          <button className="submit-btn play-again" onClick={playAgain}>
+            Play Again
+          </button>
+        </div>
+      )}
+    </div>
   );
 }
